725ab82b84fc5a23d21fdc7fd0953694d98b8561
Ivan Solenko <ivan@solenko.ru>
2019-06-29 21:19:55 +0300

    .

--- app.py
+++ app.py
@@ -12,5 +12,24 @@ def index():
     })
 
 
+@app.route('/api/userToProf')
+def profile():
+    return jsonify({
+        'labels': ['JS', 'React', 'Node.js', 'CSS', 'HTML', 'Vue.js'],
+        'datasets': [
+            {
+                'label': 'Your skills',
+                'backgroundColor': 'rgba(13, 85, 255, 0.5)',
+                'data': [40, 39, 10, 40, 39, 80]
+            },
+            {
+                'label': 'Required skills',
+                'backgroundColor': 'rgba(19, 189, 123, 0.5)',
+                'data': [100, 100, 100, 100, 100, 100]
+            }
+        ]
+    })
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
